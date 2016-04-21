ede0c68e5e9f07badd8be6addddbdd95c849e078
Nicole Wade <Nicole.Wade@target.com>
2016-04-20 20:45:59 -0500

    c2-Configured Firebase connection and tested list

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,11 +6,11 @@ var app = angular.module("nicoleBlocitoff", ["firebase"]);
 app.controller("SampleCtrl", function($scope, $firebaseObject) {
   var ref = new Firebase("https://shining-fire-4626.firebaseio.com");
   // download the data into a local object
-  var syncObject = $firebaseObject(ref);
+     var syncObject = $firebaseObject(ref);
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
   syncObject.$bindTo($scope, "data");
   // putting a console.log here won't work, see below
     
-    window.foo = $scope.data
+    window.foo = syncObject;
 });
